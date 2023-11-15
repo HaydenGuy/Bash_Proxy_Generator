94dff38e80214af7b05dc46cb1192f41ebc682b3
desktop <haydenguyn@gmail.com>
2023-11-14 19:17:49 -0800

    Changed the case logic to set the width height based on the user choice

--- proxy_generator.sh
+++ proxy_generator.sh
@@ -17,26 +17,28 @@ fi
 PS3="What resolution would you like to convert to? "
 options=("High-Res:1920x1080" "Mid-Res:1280x720" "Low-Res:854x480" "Custom" "Exit")
 
-resolution=""
+width=""
+height=""
+            
 
 select choice in "${options[@]}"; do
     case "$choice" in
         "High-Res:1920x1080")
-            resolution="high"
+            width="1920"
+            height="1080"
             break
             ;;
         "Mid-Res:1280x720")
-            resolution="mid"
+            width="1280"
+            height="720"
             break
             ;;
         "Low-Res:854x480")
-            resolution="low"
+            width="854"
+            height="480"
             break
             ;;
         "Custom")
-            width=""
-            height=""
-            
             echo "Please enter a positive width and height. Type quit to exit the application"
             while ! [[ $width =~ ^[1-9][0-9]*$ && $height =~ ^[1-9][0-9]*$ ]]; do
                 read -p "Resolution Width: " width
@@ -46,7 +48,7 @@ select choice in "${options[@]}"; do
                 fi
 
                 read -p "Resolution Height: " height
-                
+
                 if [[ $height == "quit" ]]; then
                     exit 1
                 fi
