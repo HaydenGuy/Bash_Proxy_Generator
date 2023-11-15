bdab2fd10c7f778843e7f258039bbd6989fd8c20
desktop <haydenguyn@gmail.com>
2023-11-14 19:03:30 -0800

    Added select and case for each resolution option

--- proxy_generator.sh
+++ proxy_generator.sh
@@ -14,5 +14,35 @@ else
     exit 1
 fi
 
-PS3="What resolution would you like to convert to?"
-options=("High-Res: 1920x1080" "Mid-Res: 1280x720" "Low-Res: 854x480" "Custom")
+PS3="What resolution would you like to convert to? "
+options=("High-Res:1920x1080" "Mid-Res:1280x720" "Low-Res:854x480" "Custom" "Exit")
+
+resolution=""
+
+select choice in "${options[@]}"; do
+    case "$choice" in
+        "High-Res:1920x1080")
+            resolution="high"
+            break
+            ;;
+        "Mid-Res:1280x720")
+            resolution="mid"
+            break
+            ;;
+        "Low-Res:854x480")
+            resolution="low"
+            break
+            ;;
+        "Custom")
+            read -p "Resolution Width: " width
+            read -p "Resolution Height: " height
+            break
+            ;;
+        "Exit")
+            exit 1
+            ;;
+        *)
+            echo -e "\nInvalid option. Please enter a number from 1 to ${#options[@]}\n"
+            ;;
+    esac
+done
